5c53dca289d869256486933f90e3652bb63bc513
Spencer <sh8806@engr.ship.edu>
2022-10-15 02:15:28 -0400

    Attachments WIP

--- src/main/java/weapon/PowerBooster.java
+++ src/main/java/weapon/PowerBooster.java
@@ -4,14 +4,23 @@ import exceptions.AttachmentException;
 
 public class PowerBooster extends Attachment {
   
+  public double damage = 0;
+  
   public PowerBooster(Weapon baseWeapon) throws AttachmentException {
     //makes damage = damage * (1+ (current ammo/max ammo)
-     
+    if(baseWeapon.getNumAttachments() <= 2) {
+      base = baseWeapon;
+      damage = baseWeapon.getBaseDamage() * (baseWeapon.getCurrentAmmo()
+          / baseWeapon.getMaxAmmo());
+    } else {
+      throw new AttachmentException("Already 2 Attachments!");
+    }
   }
   
   public int fire(int distance) {
-    
-    return 0;
+    int damageInt = 0;
+    damageInt = Double.valueOf(Math.floor(damage)).intValue();
+    return damageInt;
   }
   
   public String toString() {
--- src/main/java/weapon/Stabilizer.java
+++ src/main/java/weapon/Stabilizer.java
@@ -4,14 +4,25 @@ import exceptions.AttachmentException;
 
 public class Stabilizer extends Attachment {
 
+  public double damage = 0;
+  
   public Stabilizer(Weapon baseWeapon) throws AttachmentException {
     
     //auto reloads if ammo is at 0 after firing. Also increases damage by 25%
+    base = baseWeapon;
+    damage = baseWeapon.getBaseDamage();
   }
   
   public int fire(int distance) {
     
-    return 0;
+    damage = Math.floor(base.getBaseDamage() * 1.25);
+    
+    if(base.getCurrentAmmo() == 0) {
+      base.reload();
+    }
+    int damageInt = 0;
+    damageInt = Double.valueOf(Math.floor(damage)).intValue();
+    return damageInt;
   }
   
   public String toString() {
