3a1cc05df9860656dc84dd57271b7ad52153de2a
ethan <ethan@localhost>
2022-10-16 17:53:40 -0400

    Corrected took out damage

--- src/main/java/weapon/Pistol.java
+++ src/main/java/weapon/Pistol.java
@@ -24,22 +24,21 @@ public class Pistol extends GenericWeapon {
     if(distance < 0) {
       throw new WeaponException("Cannot be a negative distance");
     }
-    int damage = 0;
     if(currentAmmo == 0) {
-      return damage;
+      return 0;
     }
     if (distance > maxRange) {
       currentAmmo = currentAmmo - 1;
-      return damage;
+      return 0;
     }
     double bd = baseDamage;
     double mr = maxRange;
     double dis = distance;
     double doubleDamage = 0;
     doubleDamage = bd*((mr-dis+10)/mr);
-    damage = Double.valueOf(Math.floor(doubleDamage)).intValue();
+    baseDamage = Double.valueOf(Math.floor(doubleDamage)).intValue();
     currentAmmo = currentAmmo - 1;
-    return damage;
+    return baseDamage;
   }
 
   @Override
