e6979ef31a1db6eae9d4f094f52b9bd82c772810
Spencer <sh8806@engr.ship.edu>
2022-10-16 16:04:58 -0400

    Scope

--- src/test/java/weapon/TestScope.java
+++ src/test/java/weapon/TestScope.java
@@ -11,24 +11,47 @@ public class TestScope {
 
   @Test
   public void testPistolScope() throws AttachmentException, WeaponException {
-//    Weapon chainGunTest = new Weapon();
-//    PowerBooster powBooster = new PowerBooster(chainGunTest);
-//    assertEquals(10, powBooster.fire(10));
+    Weapon pistolTest = new Pistol(); 
+    Scope s = new Scope(pistolTest);
+    assertEquals(10, s.getBaseDamage());
+    assertEquals(60, s.getMaxRange());
+    assertEquals(20, s.fire(0));
+    assertEquals(19, s.fire(5));
+//    assertEquals(15, s.fire(30));
+    assertEquals(12, s.fire(45));
+    assertEquals(15, s.fire(55));
+    assertEquals(15, s.fire(60));
+    assertEquals(0, s.fire(61));
   }
   
   @Test
-  public void testPistolDoubleScope() {
-    
+  public void testPistolDoubleScope() throws WeaponException, AttachmentException {
+    Weapon pistolTest = new Pistol(); 
+    Scope s = new Scope(new Scope(pistolTest));
+    assertEquals(40, s.fire(0));
+    assertEquals(38, s.fire(5));
+    assertEquals(31, s.fire(30));
+    assertEquals(15, s.fire(55));
+    assertEquals(15, s.fire(60));
+    assertEquals(0, s.fire(61));
   }
   
   @Test
   public void testChainGunPowBScope() {
-    
+//    assertEquals(x, s.fire(0));
+//    assertEquals(x, s.fire(5));
+//    assertEquals(x, s.fire(30));
+//    assertEquals(x, s.fire(55));
+//    assertEquals(x, s.fire(60));
   }
   
   @Test
   public void testPlasmaStabilScope() {
-    
+//    assertEquals(x, s.fire(0));
+//    assertEquals(x, s.fire(5));
+//    assertEquals(x, s.fire(30));
+//    assertEquals(x, s.fire(55));
+//    assertEquals(x, s.fire(60));
   }
   
 
