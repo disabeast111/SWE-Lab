de06e771df2b4cecf30361fa6c2d72660c70adc4
David Wolfe <dw2214@engr.ship.edu>
2022-10-18 10:08:25 -0400

    100% instTest

--- src/main/java/weapon/Scope.java
+++ src/main/java/weapon/Scope.java
@@ -4,17 +4,16 @@ import exceptions.AttachmentException;
 import exceptions.WeaponException;
 
 public class Scope extends Attachment {
-  
+
   public double damage = 0;
   public double maxRange = 0;
   public double targetDistance = 0;
 
-  
   public Scope(Weapon baseWeapon) throws AttachmentException {
-    //increases max range of a weapon by 10 *done*
-    //also new damage = damage*(1+((new max range-distance)/new max range))
-    //if old range < target distance <= new range + 10, then damage is 5 + 
-    //base damage at max range 
+    // increases max range of a weapon by 10 *done*
+    // also new damage = damage*(1+((new max range-distance)/new max range))
+    // if old range < target distance <= new range + 10, then damage is 5 +
+    // base damage at max range
     base = baseWeapon;
     damage = baseWeapon.getBaseDamage();
     maxRange = getMaxRange();
@@ -22,19 +21,19 @@ public class Scope extends Attachment {
       throw new AttachmentException("Can not have more than 2 attachments.");
     }
   }
-  
+
   public int fire(int distance) throws WeaponException {
-    if(base.getMaxRange() < distance && distance <= maxRange) {
+    if (base.getMaxRange() < distance && distance <= maxRange) {
       return base.fire(base.getMaxRange()) + 5;
-      
-      //cannot fire with no shots left
-    } else if(distance <= base.getMaxRange()) {
-        targetDistance = distance;
-        damage = base.fire(distance);
-        
-        damage *= 1 + ((maxRange - targetDistance) / maxRange);
-        
-        return Double.valueOf(Math.floor(damage)).intValue();
+
+      // cannot fire with no shots left
+    } else if (distance <= base.getMaxRange()) {
+      targetDistance = distance;
+      damage = base.fire(distance);
+
+      damage *= 1 + ((maxRange - targetDistance) / maxRange);
+
+      return Double.valueOf(Math.floor(damage)).intValue();
     } else {
       base.fire(distance);
       return 0;
