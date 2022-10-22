594658b2b773c47e1534c9157630cc9ee1487661
Spencer <sh8806@engr.ship.edu>
2022-10-22 13:23:49 -0400

    Fixed Cell removeWeapon

--- src/main/java/environment/Cell.java
+++ src/main/java/environment/Cell.java
@@ -18,7 +18,7 @@ public class Cell {
   /**
    * addLifeForm
    * 
-   * @param entity the lifeform
+   * @param entity the LifeForm
    * @return boolean
    */
   public boolean addLifeForm(LifeForm entity) {
@@ -29,12 +29,17 @@ public class Cell {
     return false;
   }
   
+  /**
+   * Adds up to two weapons to the Cell
+   * @param weapon the weapon to be added.
+   * @return true for a weapon added, else false
+   */
   public boolean addWeapon(Weapon weapon) {
-    if(getWeaponsCount() == 0) {
+    if(weapon1 == null) {
       weapon1 = weapon;
       count += 1;
       return true;
-    } else if(getWeaponsCount() == 1) {
+    } else if(weapon2 == null) {
       weapon2 = weapon;
       count += 1;
       return true;
@@ -42,36 +47,66 @@ public class Cell {
       return false;
   }
   
+  /**
+   * LifeForm getter
+   * @return the LifeForm in the Cell
+   */
   public LifeForm getLifeForm() {
     return life1;
   }
   
+  /**
+   * Gets the weapon stored first, Weapon 1
+   * @return Weapon 1
+   */
   public Weapon getWeapon1() {
     return weapon1;
   }
   
+  /**
+   * Gets the weapon stored second, Weapon 2
+   * @return Weapon 2
+   */
   public Weapon getWeapon2() {
     return weapon2;
   }
   
+  /**
+   * Number of Weapons in the Cell getter
+   * @return Number of Weapons in the Cell
+   */
   public int getWeaponsCount() {
     return count;
   }
   
+  /**
+   * Removes the LifeForm in the Cell
+   */
   public void removeLifeForm() {
     life1 = null;
   }
   
+  /**
+   * Removes a specific Weapon from the Cell,
+   * if it currently exists there.
+   * @param weapon The Weapon to be removed
+   * @return The Weapon removed
+   */
   public Weapon removeWeapon(Weapon weapon) {
+    Weapon removed;
     if(getWeaponsCount() > 0) { 
       if(weapon1 == weapon) {
+        removed = weapon1;
+        weapon1 = null;
         count -= 1;
-        return weapon1;
+        return removed;
       } else if(weapon2 == weapon) {
+        removed = weapon2;
+        weapon2 = null;
         count -= 1;
-        return weapon;
+        return removed;
       }
     }
-    return null;
+  return null;
   }
 }
