a071ca2b02ef8ec0e2555af5c1d9d1e1bbf3d95e
Spencer <sh8806@engr.ship.edu>
2022-10-24 19:38:17 -0400

    Env exception checkstyle

--- src/main/java/exceptions/EnvironmentException.java
+++ src/main/java/exceptions/EnvironmentException.java
@@ -8,6 +8,6 @@ public class EnvironmentException extends Exception {
   private static final long serialVersionUID = 4L;
 
   public EnvironmentException(String message) {
-      super();
+    super();
   }
 }
