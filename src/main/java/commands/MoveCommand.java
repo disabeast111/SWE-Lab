3c5ee17d25d706cea5b084da3a4ee96a3f3ff321
ethan <ej0115@ship.edu>
2022-11-10 19:45:26 -0500

    Fix Commit

--- src/main/java/commands/MoveCommand.java
+++ src/main/java/commands/MoveCommand.java
@@ -16,7 +16,7 @@ public class MoveCommand implements Command {
 
   @Override
   public void execute() {
-    // TODO Auto-generated method stub
+    
     speed = lifeForm.getMaxSpeed();
     enviro.move(lifeForm);
     
