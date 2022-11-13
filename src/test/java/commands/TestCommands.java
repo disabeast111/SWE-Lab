10f93795200d8623c28aeeb7cf2084082d31aa47
ethan <ej0115@ship.edu>
2022-11-13 14:43:45 -0500

    Added test for all four turn directions

--- src/test/java/commands/TestCommands.java
+++ src/test/java/commands/TestCommands.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import lifeform.MockLifeForm;
+
+
 public class TestCommands {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testTurnCommands() {
+    MockLifeForm entity1 = new MockLifeForm("Jim", 40, 1);
+    assertEquals(0, entity1.getCurrentDirection());
+    TurnEastCommand turnEast = new TurnEastCommand(entity1);
+    TurnNorthCommand turnNorth = new TurnNorthCommand(entity1);
+    TurnSouthCommand turnSouth = new TurnSouthCommand(entity1);
+    TurnWestCommand turnWest = new TurnWestCommand(entity1);
+    turnEast.execute();
+    assertEquals(1, entity1.getCurrentDirection());
+    turnNorth.execute();
+    assertEquals(0, entity1.getCurrentDirection());
+    turnSouth.execute();
+    assertEquals(2, entity1.getCurrentDirection());
+    turnWest.execute();
+    assertEquals(3,entity1.getCurrentDirection());
+    
   }
 
 }
