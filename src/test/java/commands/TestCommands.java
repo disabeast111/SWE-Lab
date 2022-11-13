c7ae67f966095a08664c39de14cc537fc847f6a4
ethan <ej0115@ship.edu>
2022-11-13 16:04:02 -0500

    MoveCommand test created

--- src/test/java/commands/TestCommands.java
+++ src/test/java/commands/TestCommands.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import environment.Environment;
 import exceptions.WeaponException;
+import lifeform.Human;
 import lifeform.MockLifeForm;
 import weapon.Pistol;
 import weapon.Weapon;
@@ -61,7 +63,7 @@ public class TestCommands {
    * @throws WeaponException when attempted
    */
   @Test
-  public void testNoWeaponReloaddCommand() throws WeaponException {
+  public void testNoWeaponReloadCommand() throws WeaponException {
     MockLifeForm entity1 = new MockLifeForm("Jim", 40, 1);
     ReloadCommand reloadCommand = new ReloadCommand(entity1);
     boolean caught = false;
@@ -72,7 +74,31 @@ public class TestCommands {
       caught = true;
     }
     assertTrue(caught);
-    
    
+  }
+  
+  /**
+   * Tests that movement command works and correctly moves a LifeForm
+   */
+  @Test
+  public void testMoveCommand() {
+    Environment e = Environment.getEnvironment(5, 5);
+    Human entity = new Human("Bob", 40, 0);
+    entity.setLocation(2, 2);
+    assertEquals(2, entity.getRow());
+    assertEquals(2, entity.getCol());
+    MoveCommand moveCommand = new MoveCommand(entity, e);
+    moveCommand.execute();
+    entity.updateTime(0);
+    assertEquals(0, entity.getRow());
+    assertEquals(2, entity.getCol());
+
+    TurnEastCommand turnEast = new TurnEastCommand(entity);
+    turnEast.execute();
+    moveCommand.execute();
+    entity.updateTime(0);
+    assertEquals(0, entity.getRow());
+    assertEquals(4, entity.getCol());
+    
   }
 }
