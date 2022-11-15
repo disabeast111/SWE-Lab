c2a57a35637fa1df3be62e7de3c44b4e3bb79c09
Spencer <sh8806@engr.ship.edu>
2022-11-15 00:32:55 -0500

    InvokerBuilder checkstyle

--- src/main/java/commands/InvokerBuilder.java
+++ src/main/java/commands/InvokerBuilder.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import environment.Cell;
 import environment.Environment;
 import exceptions.AttachmentException;
+import exceptions.EnvironmentException;
 import exceptions.RecoveryRateException;
 import gameplay.GameBoard;
 import lifeform.Alien;
@@ -13,6 +14,7 @@ import lifeform.LifeForm;
 import recovery.RecoveryLinear;
 import weapon.Pistol;
 import weapon.Scope;
+import weapon.Weapon;
 
 
 public class InvokerBuilder extends JFrame {
@@ -21,8 +23,13 @@ public class InvokerBuilder extends JFrame {
   
   
  
-  
-  public static void main (String[] args) throws AttachmentException, RecoveryRateException {
+  /**
+   * Main method to build everything
+   * @param args
+   * @throws AttachmentException 
+   * @throws RecoveryRateException
+   */
+  public static void main(String[] args) throws AttachmentException, RecoveryRateException {
     Environment env = Environment.getEnvironment(10, 10);
     Invoker gui = new Invoker();
     int x = 500;
@@ -33,7 +40,15 @@ public class InvokerBuilder extends JFrame {
     LifeForm jane = new Alien("Jane", 10, new RecoveryLinear(5));
     env.addLifeForm(joe, 0,0);
     env.addLifeForm(jane, 3, 6);
-    GameBoard gb = GameBoard.getInstance();
+    Weapon weap = new Scope(new Pistol());
+        
+    try {
+      env.getCell(3, 5).addWeapon(weap);
+    } catch (EnvironmentException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    GameBoard.getInstance();
   }
   
   public void setCommand(Command c) {
